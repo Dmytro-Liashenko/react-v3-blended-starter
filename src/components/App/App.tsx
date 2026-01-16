9ae71a4ada05f29c9dda4409ce157a965aefdff1
Dmytro_L <dimka4545452@gmail.com>
2026-01-16 20:12:59 +0100

    ready

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,17 +4,42 @@ import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { useState } from "react";
+import { Post } from "../../types/post";
+import { useDebouncedCallback } from "use-debounce";
+
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isCreatePost, setIsCreatePost] = useState(false)
+  const [isEditPost, setIsEditPost] = useState(false)
+  const [editedPost, setEditedPost] = useState<Post | null>(null)
+  const [searchQuery, setSearchQuery] = useState("")
+  // const [debouncedSearchQuery] = useDebouncedCallback(searchQuery, 300)
+
+
+  const {data} = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(searchQuery, currentPage),
+    placeholderData: keepPreviousData
+    
+  })
+
+
+
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox />
-        <Pagination />
+        {/* <SearchBox /> */}
+        {/* <Pagination /> */}
         <button className={css.button}>Create post</button>
       </header>
       <Modal>{/* Передати через children компонент CreatePostForm або EditPostForm */}</Modal>
-      <PostList />
+      {data && data?.length > 0 && <PostList posts={data} toggleModal={() => {}} toggleEditPost={()=>{}}/>}
     </div>
   );
 }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
+import { Post } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-export const fetchPosts = async (searchText, page) => {};
+export const fetchPosts = async (searchText: string, page: number) :Promise<Post[]> => {
+    const res = await axios.get<Post[]>("/posts", {params:{
+        ...(searchText !== "" && {q: searchText}),
+        _page: page,
+        _limit: 8,
+    }})
+    return res.data
+};
 
 export const createPost = async (newPost) => {};
 
 export const editPost = async (newDataPost) => {};
 
-export const deletePost = async (postId) => {};
+export const deletePost = async (postId: number) => {
+    const res = await axios.delete<Post>(`/posts/${postId}`)
+    return res.data
+};
